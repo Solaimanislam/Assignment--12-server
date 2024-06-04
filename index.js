180d92d7bd04ee1676573e6e9b05bf6deaa6ff02
Solaimanislam <islamsi2001@gmail.com>
2024-06-05 02:53:55 +0600

    added verify token

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config()
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 
@@ -10,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ysdrtdj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,12 +28,85 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
 
+        const userCollection = client.db("DiagnosticDB").collection('users');
         const testCollection = client.db("DiagnosticDB").collection('test');
         const bookedCollection = client.db("DiagnosticDB").collection('booked');
 
+        // JWT related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ token });
+        })
+
+        // middle ware
+        const verifyToken = (req, res, next) => {
+            console.log('inside verify token', req.headers.authorization);
+            if (!req.headers.authorization) {
+                res.status(401).send({ message: 'unauthorized access  ' });
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            console.log(token);
+            console.log(process.env.ACCESS_TOKEN_SECRET);
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'unauthorized access 51' })
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
+
+        // users related api
+
+        app.get('/users', async (req, res) => {
+
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+        })
+
+
+        app.patch('/users/active/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    status: 'blocked'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            // insert email if user does not exists
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'User already exists', insertedId: null })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
         // test related api
         app.get('/test', async (req, res) => {
-            const result = await testCollection.find().toArray();
+            const email = req.query.email;
+            const query = { email: email }
+            const result = await testCollection.find(query).toArray();
             res.send(result);
         })
 
