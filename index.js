1b6c1feba413e5483a2d35c7133f1cf8caf9351d
Solaimanislam <islamsi2001@gmail.com>
2024-06-10 23:06:27 +0600

    added jwt

--- index.js
+++ index.js
@@ -68,6 +68,7 @@ async function run() {
             const query = { email: email }
             const user = await userCollection.findOne(query);
             const isAdmin = user?.role === 'admin';
+            clg
             if (!isAdmin) {
                 return res.status(403).send({ message: 'forbidden access' });
             }
@@ -353,8 +354,8 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
